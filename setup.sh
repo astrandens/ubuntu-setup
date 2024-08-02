1e068db6120bf801e38d24d945eee7f70f69f02f
astrandens <alexander.astrand@saferadar.se>
2024-08-02 08:49:48 +0200

    Add lazygit

--- setup.sh
+++ setup.sh
@@ -57,3 +57,12 @@ if ! hash pipx 2>/dev/null; then
 fi
 
 git config --global core.editor "nvim"
+
+if ! hash lazygit 2>/dev/null; then
+  # Ref: https://github.com/jesseduffield/lazygit?tab=readme-ov-file#installation
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+  rm lazygit lazygit.tar.gz
+fi
