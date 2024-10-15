ad8e3e4b8fedf3c441b20262593f386f87b3c6b4
aastrand <alexander.astrand@saferadar.se>
2024-10-15 13:38:14 +0200

    Update nvim configuration

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,4 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.relativenumber = true -- Relative line numbers
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.pumheight = 5 -- Maximum number of entries in a popup
+opt.relativenumber = false -- Relative line numbers
